51b2aee3fa991e49497ef070e81291158689c7c8
Lucas Peterson <lucpeter@mymail.mines.edu>
2017-10-12 21:16:44 -0600

    js additions and styling

--- drawLoop.js
+++ drawLoop.js
@@ -1,56 +1,70 @@
 var canvas = document.getElementById('game_canvas');
 var context = canvas.getContext('2d');
+var damage = $(".selectedTower").attr("data-damage");
+var towerColor = $(".selectedTower").attr("data-color");
 
 var initialPosition = [0,1];
 var pathLocations = [];
 function populatePath() { 
-    var path = [['r', 17],['d', 4],['l', 15],['d', 4],['r', 10],['d', 12]];
-    x = initialPosition[0];
-    y = initialPosition[1];
-    pathLocations.push([x, y]);
-    for (i = 0, len = path.length; i < len; i++) { 
-        for (j = path[i][1]; j > 0; j--) {
-            switch(path[i][0]) {
-                case 'l':
-                    x--;
-                    break;
-                case 'r':
-                    x++;
-                    break;
-                case 'd':
-                    y++;
-                    break;
-                case 'u':
-                    y--;
-             }
-             pathLocations.push([x,y]);
-        }
+  var path = [['r', 17],['d', 4],['l', 15],['d', 4],['r', 10],['d', 12]];
+  x = initialPosition[0];
+  y = initialPosition[1];
+  pathLocations.push([x, y]);
+  for (i = 0, len = path.length; i < len; i++) { 
+    for (j = path[i][1]; j > 0; j--) {
+      switch(path[i][0]) {
+        case 'l':
+          x--;
+          break;
+        case 'r':
+          x++;
+          break;
+        case 'd':
+          y++;
+          break;
+        case 'u':
+          y--;
+      }
+      pathLocations.push([x,y]);
     }
-    drawPath();
+  }
+  drawPath();
 }
 
 bricks = new Image();
 function drawPath(){
-    for (i = 0, len = pathLocations.length; i < len; i++) {
-        context.drawImage(bricks, pathLocations[i][0]*25, pathLocations[i][1]*25);
-    }
+  for (i = 0, len = pathLocations.length; i < len; i++) {
+    context.drawImage(bricks, pathLocations[i][0]*25, pathLocations[i][1]*25);
+  }
 }
 bricks.src = "images/bricks.png";
 
 function drawTowers() {
-    var centerX;
-    var centerY;
-    var radius = 8;
+  var centerX;
+  var centerY;
+  var radius = 8;
 
-    for(i = 0, len = towerLocations.length; i < len; i++) {
-        centerX = towerLocations[i][0]*25 + 13;
-        centerY = towerLocations[i][1]*25 + 13;
-        context.beginPath();
-        context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-        context.fillStyle = 'green';
-        context.fill();
-        context.lineWidth = 5;
-        context.strokeStyle = '#003300';
-        context.stroke();
-    }
+  for(i = 0, len = towerLocations.length; i < len; i++) {
+    centerX = towerLocations[i][0]*25 + 13;
+    centerY = towerLocations[i][1]*25 + 13;
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = '#003300';
+    context.stroke();
+  }
 }
+
+$(".towerButton").on('click', function() {
+  $(this).addClass("selectedTower");
+  $(".selectedTower").removeClass("selectedTower");
+  $(this).addClass("selectedTower");
+  damage = $(this).data("data-damage");
+  towerColor = $(this).attr("data-color");
+})
+
+$("#startBtn").on('click', function() {
+  //Add start button calls
+})
